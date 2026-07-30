c51f2d934602334d7c8123781e853a792f3d5f31
reemyousef2002 <reemyosef2002@gmail.com>
2026-07-30 13:34:57 +0300

    Delete doctors directory

--- doctors/doctors/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-
-from .views import DoctorViewSet
-
-router = DefaultRouter()
-router.register(r"doctors", DoctorViewSet, basename="doctor")
-
-urlpatterns = router.urls
