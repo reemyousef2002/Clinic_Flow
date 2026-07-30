d77b1c514a9e20f93098a12d3c17fff700858dd6
reemyousef2002 <reemyosef2002@gmail.com>
2026-07-30 13:35:19 +0300

    Delete medicines directory

--- medicines/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-
-from .views import MedicineViewSet
-
-router = DefaultRouter()
-router.register(r"medicines", MedicineViewSet, basename="medicine")
-
-urlpatterns = router.urls
