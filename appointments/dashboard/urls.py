341cf39c99b9b628576087c7409507f1fc45ec97
reemyousef2002 <reemyosef2002@gmail.com>
2026-07-30 13:34:30 +0300

    Delete appointments directory

--- appointments/dashboard/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from .views import DashboardStatsView
-
-urlpatterns = [
-    path("dashboard/stats/", DashboardStatsView.as_view(), name="dashboard-stats"),
-]
--- appointments/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-
-from .views import AppointmentViewSet
-
-router = DefaultRouter()
-router.register(r"appointments", AppointmentViewSet, basename="appointment")
-
-urlpatterns = router.urls
